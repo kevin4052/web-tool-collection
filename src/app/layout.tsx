b9ff1ea1ceab965e406a5e6fd443ea38041c9ee4
kevin4052 <kevin.hernandez001@gmail.com>
2024-10-17 17:27:03 -0500

    prettier, refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,33 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
-import Link from "next/link";
-import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
-
+import { NavBar } from "@/components/nav-bar";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
-  title: "Web Tool Collection",
-  description: "Collection of tools to help with web development",
+	title: "Web Tool Collection",
+	description: "Collection of tools to help with web development",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <header className="py-3 px-2 flex sticky top-0 border-b-2 backdrop-blur-sm">
-            <div className="flex-1 font-bold content-center">
-              <Link href={'/'} className="flex flex-row gap-2">
-                <AdjustmentsHorizontalIcon className="w-6 h-6" />
-                <span>WebTools Collection</span>
-              </Link>
-            </div>
-            <div>
-              <ModeToggle />
-            </div>
-          </header>
-          <main>{children}</main>
-          <footer className="py-3 px-2 border-t-2">footer</footer>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<NavBar />
+					<main>{children}</main>
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,3 @@
+export function Footer() {
+	return <footer className="py-3 px-2 border-t-2">footer</footer>;
+}
