c459f1e33a40e1796b1b250463ed14454483ac69
kevin4052 <kevin.hernandez001@gmail.com>
2024-11-07 10:12:12 -0600

    qrcode download

--- src/app/tools/(generators)/qrcode-generator/page.tsx
+++ src/app/tools/(generators)/qrcode-generator/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ToolWrapper from "../../_components/tool-wrapper";
 import { Card, CardContent } from "@/components/ui/card";
 import InputTextArea from "@/components/input-text-area";
@@ -10,6 +10,13 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 const toolData = {
 	name: "QR Code Generator",
@@ -18,31 +25,83 @@ const toolData = {
 };
 
 export default function QrCodeGenerator() {
+	const svgRef = useRef<SVGSVGElement>(null);
 	const [input, setInput] = useState(
 		"https://web-tool-collection-production.up.railway.app/"
 	);
-	const [imageSize, setImageSize] = useState([256]);
+	const [imageSize, setImageSize] = useState([500]);
 	const [margin, setMargin] = useState([1]);
+	const [fileType, setFileType] = useState("");
 
-	// useEffect(() => {
-	// 	handleImageUpdate();
-	// }, []);
+	const downloadStringAsFile = (data: string, fileName: string) => {
+		const a = document.createElement("a");
+		a.download = fileName;
+		a.href = data;
+		a.click();
+	};
 
-	// const handleImageUpdate = () => {
-	// 	const svgNode = document.getElementById("qrSvg");
-	// 	const svgString = new XMLSerializer().serializeToString(
-	// 		svgNode as HTMLElement
-	// 	);
-	// 	const svgUrl = `data:image/svg+xml;charset=utf-8;base64,${btoa(svgString)}`;
-	// 	const image = new Image();
-	// };
+	const downloadPNG = (svgData: string) => {
+		const canvas = document.createElement("canvas");
+		const ctx = canvas.getContext("2d");
+		const img = new Image();
 
-	const handleImageDownload = () => {};
+		img.onload = () => {
+			const borderSize = 10;
+			const borderColor = "#ffffff";
+
+			canvas.width = img.width + 2 * borderSize;
+			canvas.height = img.height + 2 * borderSize;
+
+			ctx!.fillStyle = borderColor;
+			ctx!.fillRect(0, 0, canvas.width, canvas.height);
+
+			const xPosition = (canvas.width - img.width) / 2;
+			const yPosition = (canvas.height - img.height) / 2;
+			ctx!.drawImage(img, xPosition, yPosition);
+
+			const pngFile = canvas.toDataURL("image/png");
+
+			downloadStringAsFile(pngFile, "qrcode.png");
+		};
+
+		img.src = `data:image/svg+xml;base64,${Buffer.from(svgData).toString(
+			"base64"
+		)}`;
+	};
+
+	const downloadSVG = (data: string) => {
+		const fileURI =
+			"data:image/svg+xml;charset=utf-8," +
+			encodeURIComponent('<?xml version="1.0" standalone="no"?>' + data);
+
+		downloadStringAsFile(fileURI, "qrcode.svg");
+	};
+
+	const handleImageDownload = () => {
+		if (fileType === "") {
+			return;
+		}
+
+		const svgNode = svgRef.current;
+		if (svgNode === null) {
+			return;
+		}
+		const svgData = new XMLSerializer().serializeToString(svgNode);
+
+		switch (fileType) {
+			case "SVG":
+				downloadSVG(svgData);
+				break;
+			case "PNG":
+				downloadPNG(svgData);
+				break;
+		}
+	};
 
 	return (
 		<ToolWrapper name={toolData.name} description={toolData.description}>
 			<Card>
-				<CardContent className="flex md:flex-row flex-col gap-4 py-4">
+				<CardContent className="flex md:flex-row flex-col gap-4 py-4 max-w-full">
 					<div className="flex flex-col flex-1 gap-4">
 						<InputTextArea
 							title="QR Code Text"
@@ -94,13 +153,15 @@ export default function QrCodeGenerator() {
 								<div className="aspect-square flex justify-center items-center">
 									{input && (
 										<QRCodeSVG
+											ref={svgRef}
 											value={input}
 											size={imageSize[0]}
 											level={"L"}
 											bgColor={"#FFFFFF"}
 											fgColor={"#000000"}
 											marginSize={margin[0]}
-											className="h-[300px] md:h-[380px]"
+											className="aspect-square h-[300px] md:h-[380px]"
+											id="qrSvg"
 										/>
 									)}
 								</div>
@@ -118,6 +179,18 @@ export default function QrCodeGenerator() {
 										onValueChange={(value) => setImageSize(value)}
 										className="my-2"
 									/>
+								</div>
+								<div className="mt-4">
+									<Label>File type</Label>
+									<Select onValueChange={(value) => setFileType(value)}>
+										<SelectTrigger className="w-full">
+											<SelectValue placeholder="Select type" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectItem value="SVG">svg</SelectItem>
+											<SelectItem value="PNG">png</SelectItem>
+										</SelectContent>
+									</Select>
 								</div>
 							</CardContent>
 						</Card>
--- src/app/tools/_components/tool-wrapper.tsx
+++ src/app/tools/_components/tool-wrapper.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ToolWrapper({ name, description, children }: Props) {
 	return (
-		<div className="min-h-screen w-full mx-auto max-w-[1680px] flex flex-col md:px-6 px-3">
+		<div className="min-h-screen w-full mx-auto mb-4 max-w-[1680px] flex flex-col md:px-6 px-3">
 			<div className="flex flex-col my-8">
 				<h3 className="font-semibold text-2xl">{name}</h3>
 				<p>{description}</p>
