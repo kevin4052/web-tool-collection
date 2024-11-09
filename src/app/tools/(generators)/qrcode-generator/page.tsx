33734e138fc3652a5afda1671256e30c1094bb4f
kevin4052 <kevin.hernandez001@gmail.com>
2024-11-09 12:43:22 -0600

    finishing qr code page sliders

--- src/app/tools/(generators)/qrcode-generator/page.tsx
+++ src/app/tools/(generators)/qrcode-generator/page.tsx
@@ -17,12 +17,15 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
 
 const toolData = {
 	name: "QR Code Generator",
 	description:
 		"Generates QR codes in real time that can be used for a variety of purposes",
 };
+
+type ErrorCorrectionLevel = "L" | "M" | "Q" | "H";
 
 export default function QrCodeGenerator() {
 	const svgRef = useRef<SVGSVGElement>(null);
@@ -32,6 +35,7 @@ export default function QrCodeGenerator() {
 	const [imageSize, setImageSize] = useState([500]);
 	const [margin, setMargin] = useState([1]);
 	const [fileType, setFileType] = useState("");
+	const [errorLvl, setErrorLvl] = useState<ErrorCorrectionLevel>("M");
 
 	const downloadStringAsFile = (data: string, fileName: string) => {
 		const a = document.createElement("a");
@@ -105,7 +109,13 @@ export default function QrCodeGenerator() {
 							<CardContent className="flex flex-col gap-4 py-4">
 								<div>
 									<Label>Error Correction Level</Label>
-									<RadioGroup defaultValue="M" className="flex flex-row gap-4">
+									<RadioGroup
+										defaultValue="M"
+										className="flex flex-row gap-4 py-2"
+										onValueChange={(value: ErrorCorrectionLevel) =>
+											setErrorLvl(value)
+										}
+									>
 										<div className="flex items-center space-x-2">
 											<RadioGroupItem value="L" id="r1" />
 											<Label htmlFor="r1">L</Label>
@@ -126,15 +136,25 @@ export default function QrCodeGenerator() {
 								</div>
 								<div>
 									<Label>Margin</Label>
-									<Slider
-										defaultValue={margin}
-										max={20}
-										min={0}
-										step={1}
-										onValueChange={(value) => setMargin(value)}
-									/>
+									<div className="flex flex-row gap-4">
+										<Slider
+											defaultValue={margin}
+											max={20}
+											min={0}
+											step={1}
+											value={margin}
+											onValueChange={(value) => setMargin(value)}
+										/>
+										<Input
+											className="w-[70px] px-3"
+											value={margin[0]}
+											onChange={(value) => setMargin([+value.target.value])}
+											type="number"
+											min={0}
+											max={20}
+										/>
+									</div>
 								</div>
-								<div>QR Code Color</div>
 							</CardContent>
 						</Card>
 					</div>
@@ -149,7 +169,7 @@ export default function QrCodeGenerator() {
 											size={imageSize[0]}
 											width={"100%"}
 											height={"100%"}
-											level={"L"}
+											level={errorLvl}
 											bgColor={"#FFFFFF"}
 											fgColor={"#000000"}
 											marginSize={margin[0]}
@@ -162,15 +182,26 @@ export default function QrCodeGenerator() {
 						<Card>
 							<CardContent className="py-4">
 								<div>
-									<Label>Image Size {imageSize[0]}</Label>
-									<Slider
-										defaultValue={imageSize}
-										max={1024}
-										min={50}
-										step={1}
-										onValueChange={(value) => setImageSize(value)}
-										className="my-2"
-									/>
+									<Label>Image Size</Label>
+									<div className="flex flex-row gap-4">
+										<Slider
+											defaultValue={imageSize}
+											value={imageSize}
+											max={1024}
+											min={50}
+											step={1}
+											onValueChange={(value) => setImageSize(value)}
+											className="my-2"
+										/>
+										<Input
+											className="w-[80px] px-3"
+											value={imageSize[0]}
+											onChange={(value) => setImageSize([+value.target.value])}
+											type="number"
+											min={50}
+											max={1024}
+										/>
+									</div>
 								</div>
 								<div className="mt-4">
 									<Label>File type</Label>
